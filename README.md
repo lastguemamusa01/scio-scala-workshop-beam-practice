4addd770d789cd9bb8a902c85cb9de29838ab739
min ku nam <minku.nam@dexcom.com>
2022-11-01 22:00:42 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# Dataflow Streaming example with Scio
+
+This is the example used in the Scio/Scala workshop in the Beam Summit 2021
+
+* https://2021.beamsummit.org/sessions/scala-streaming-pipeline-using-scio/
+* [Slides in PDF](./docs/beam_summit_2021_scio_workshop.pdf)
+
+Branches in this repo:
+
+* `master`, wih incomplete code with some exercises
+* `solution`, with the solution to the exercises
+
+## Description of the pipeline
+
+This pipeline takes some JSON data from a public PubSub topic, parses the JSON strings into a custom case class,
+aggregates some of the produced objects, and writes some output to BigQuery.
+
+You need to have a project in Google Cloud Platform in order to run this pipeline.
+
+## How to launch the pipeline
+
+Run `sbt stage` to generate a package for the pipeline, and the use the launch script with your project name
+
+`$ ./launch.sh $MY_PROJECT_NAME`
+
+You should have installed `gcloud` authenticated with the necessary credentials, or those credentials should be
+available in the environment variable `GOOGLE_APPLICATION_CREDENTIALS`. For more details
+see https://cloud.google.com/docs/authentication/getting-started
+
+In the Dataflow UI, you should a graph of the pipeline like this one:
+
+![Graph of the pipline](./imgs/pipeline.png)
+
+The script assumes that you want to launch the pipeline in `us-east1` and that you have the following resources in
+your project already created
+
+* A BigQuery dataset called `taxi_rides`
+* A Cloud Storage bucket called `gs://${MY_PROJECT_NAME}`
+
+You should create both resources, in the same region as where you are launching the pipeline.
+
+The launch script will delete tables in your BigQuery dataset, so use a new sandbox dataset to try this pipeline out.
+
+### Input data
+
+The pipeline reads data from this public PubSub topic:
+
+`projects/pubsub-public-data/topics/taxirides-realtime`
+
+### Output data
+
+The pipeline will create three tables in BigQuery:
+
+* `rides`, with the same schema and contents as the incoming messages
+* `agg`, with Taxi rides aggregated together
+* `errors`, with details about any message that could not be parsed from the input data source.
+
+## Testing
+
+The pipeline comes with a set of unit tests. To run them, please do
+
+```
+sbt test
+```
+
+In the master branch, one test will fail because one of the exercises is incomplete.
+
+## License
+
+Copyright 2011 Israel Herraiz
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "
+AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+language governing permissions and limitations under the License.
+# scio-scala-workshop-beam-practice
---
+++ src/main/scala/dev/herraiz/TaxiSessionsPipeline.scala
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2021 Israel Herraiz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.herraiz
+
+import com.spotify.scio.bigquery._
+import com.spotify.scio.bigquery.{CREATE_IF_NEEDED, Table, WRITE_TRUNCATE}
+import com.spotify.scio.pubsub.PubsubIO
+import com.spotify.scio.values.{SCollection, WindowOptions}
+import com.spotify.scio.{Args, ContextAndArgs, ScioContext, streaming}
+import dev.herraiz.data.DataTypes._
+import io.circe
+import org.apache.beam.sdk.transforms.windowing.{AfterProcessingTime, AfterWatermark}
+import org.joda.time.Duration
+
+object TaxiSessionsPipeline {
+  // seconds
+  val SESSION_GAP = 600
+  val EARLY_RESULT = 10
+  val LATENESS = 900
+
+  def main(cmdlineArgs: Array[String]): Unit = {
+    val (scontext: ScioContext, opts: Args) = ContextAndArgs(cmdlineArgs)
+    implicit val sc: ScioContext = scontext
+
+    val pubsubTopic: String = opts("pubsub-topic")
+    val goodTable = opts("output-table")
+    val badTable = opts("errors-table")
+    val accumTable = opts("accum-table")
+
+    val messages: SCollection[String] = getMessagesFromPubSub(pubsubTopic)
+    val (rides, writableErrors) = parseJSONStrings(messages)
+
+    rides.saveAsBigQueryTable(Table.Spec(goodTable), WRITE_APPEND, CREATE_IF_NEEDED)
+    writableErrors.saveAsBigQueryTable(Table.Spec(badTable), WRITE_APPEND, CREATE_IF_NEEDED)
+
+    // Group by session with a max duration of 5 mins between events
+    // Window options
+    val wopts: WindowOptions = customWindowOptions
+    val groupRides: SCollection[TaxiRide] = groupRidesByKey(rides.map(_.toTaxiRide), wopts)
+    groupRides.saveAsBigQueryTable(Table.Spec(accumTable), WRITE_APPEND, CREATE_IF_NEEDED)
+
+    sc.run
+  }
+
+  def customWindowOptions: WindowOptions =
+    WindowOptions(
+      trigger = AfterWatermark.pastEndOfWindow()
+        .withEarlyFirings(AfterProcessingTime
+          .pastFirstElementInPane
+          .plusDelayOf(Duration.standardSeconds(EARLY_RESULT)))
+        .withLateFirings(AfterProcessingTime
+          .pastFirstElementInPane()
+          .plusDelayOf(Duration.standardSeconds(LATENESS))),
+      accumulationMode = streaming.ACCUMULATING_FIRED_PANES,
+      allowedLateness = Duration.standardSeconds(LATENESS)
+    )
+
+  def getMessagesFromPubSub(pubsubTopic: String)(implicit sc: ScioContext): SCollection[String] = {
+    // ???  -> undefine code
+    val msgs: PubsubIO[String] = PubsubIO.string(pubsubTopic, timestampAttribute = "ts")
+    val param: PubsubIO.ReadParam = PubsubIO.ReadParam(PubsubIO.Topic)
+
+    /*_*/   // to tell the scala, it is ok
+    val output: SCollection[String] = sc.read(msgs)(param) /*_*/
+    output
+  }
+
+  def parseJSONStrings(messages: SCollection[String]):
+  (SCollection[PointTaxiRide], SCollection[JsonError]) = {
+    val parsed: SCollection[Either[circe.Error, PointTaxiRide]] = messages.map(json2TaxiRide)
+
+    // 0 and 1 is bucket number
+    val lefts :: rights :: Nil : Seq[SCollection[Either[circe.Error, PointTaxiRide]]] = parsed.partition(2, { e =>
+      e match {
+        case Left(_) => 0
+        case Right(_1) => 1
+      }
+    })
+
+    val errors: SCollection[JsonError] = lefts.map(e => circeErrorToCustomError(e.left.get))
+    val rides: SCollection[PointTaxiRide] = rights.map(_.right.get)
+
+    (rides, errors)
+
+  }
+
+  def groupRidesByKey(rides: SCollection[TaxiRide], wopts: WindowOptions): SCollection[TaxiRide] = {
+
+    val withKeys: SCollection[(String, TaxiRide)] = rides.keyBy(_.ride_id)
+
+    val windowed: SCollection[(String, TaxiRide)] =
+      withKeys.withSessionWindows(Duration.standardSeconds(SESSION_GAP), wopts)
+
+    // _._2 take the second element
+    val reduced: SCollection[TaxiRide] = windowed.reduceByKey(_ + _).map(_._2)
+    reduced
+  }
+}
